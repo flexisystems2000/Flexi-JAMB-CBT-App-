92c3f984b750de56e8a68ffb561cc368f70c5798
flexisystems2000 <flexisystems2000@gmail.com>
2026-08-17 14:51:19 +0100

    Recently uploaded

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'flexi-core-v2';
+
+const OFFLINE_ASSETS = [
+  './',
+  './index.html',
+  './theme.css',
+  './theme.js',
+  './idb.js',
+  './manifest.json',
+  './boxicons.js',
+  './lucide.min.js',
+  './chart.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(OFFLINE_ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
